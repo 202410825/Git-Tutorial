9e03f637f77d683c9517d24aba1339f92ccc5803
unknown <cutemj127@gmail.com>
2024-03-18 19:59:43 +0900

    Add Text File [hw1.py]

---
+++ hw1.py
@@ -0,0 +1,15 @@
+
+
+def get_radius(prompt) :
+    r = int(input(prompt))
+    return r
+
+def get_circle_area(radius) :
+    area = 3.14*(radius**2)
+    return area
+
+def show_circle_area() :
+    radius = get_radius('반지름의 길이는?')
+    area = get_circle_area(radius)
+    print('원의 넓이는:', area)
+show_circle_area()
